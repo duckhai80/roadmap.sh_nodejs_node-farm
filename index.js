54f39bf3a1184e37e158990e18bf84458fecf4ff
Truong Duc Khai <duckhai80@gmail.com>
2024-09-15 22:17:18 +0700

    Filling the templates

--- index.js
+++ index.js
@@ -4,13 +4,54 @@ const url = require("url");
 
 /* SERVER */
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObject = JSON.parse(data);
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const dataObj = JSON.parse(data);
+
+const replaceTemplate = (template, product) => {
+  let newTemplate = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+
+  newTemplate = newTemplate.replace(/{%ID%}/g, product.id);
+  newTemplate = newTemplate.replace(/{%IMAGE%}/g, product.image);
+  newTemplate = newTemplate.replace(/{%PRICE%}/g, product.price);
+  newTemplate = newTemplate.replace(/{%FROM%}/g, product.from);
+  newTemplate = newTemplate.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  newTemplate = newTemplate.replace(/{%QUANTITY%}/g, product.quantity);
+  newTemplate = newTemplate.replace(/{%DESCRIPTION%}/g, product.description);
+
+  if (!product.organic)
+    newTemplate = newTemplate.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return newTemplate;
+};
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+    const cardsHtml = dataObj
+      .map((dataItem) => replaceTemplate(templateCard, dataItem))
+      .join("");
+    const templateOverviewFilled = templateOverview.replace(
+      /{%PRODUCT_CARDS%}/g,
+      cardsHtml
+    );
+
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(templateOverviewFilled);
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
   } else if (pathName === "/api") {
